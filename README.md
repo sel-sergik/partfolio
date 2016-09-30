6b1b4130904bcf992979a62484356d4cd2ca9528
sel-sergik-ПК\sel-sergik <sel-sergik@mail.ru>
2016-09-30 09:35:21 +0200

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# partfolio
+# PARTFOLIO
+
+##Partfolio for mentoring program
+
+1. Install dependencies: `npm install`.
+2. Run `gulp`.
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', function(){
+  gulp.run('sass');
+
+  // Отслеживаем изменения в файлах
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
