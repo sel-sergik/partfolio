0efab88c8ee82bd56d6ba35eba1a598c9c37ea7f
sel-sergik-ПК\sel-sergik <sel-sergik@mail.ru>
2016-10-07 00:39:32 +0200

    add gh-pages and change settings

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var deploy = require('gulp-gh-pages');
  
 gulp.task('sass', function () {
 	gulp.src('./sass/**/*.scss')
@@ -17,11 +18,18 @@ gulp.task('autoprefixer', function () {
             browsers: ['last 3 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('dest'))
+        .pipe(gulp.dest('dist'))
 });
 
 gulp.task('default', function(){
 	gulp.run('sass');
 
 	gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('deploy', function () {
+	gulp.run('default');
+
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
 });
